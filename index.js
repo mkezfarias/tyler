5691e517fcdbbe789dae7be92fd4e82f5d3e609b
Luis Puente <puente.luism@gmail.com>
2020-07-27 20:02:05 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let dataText = document.querySelector(".data-text");
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const stringsContainer = Array.from(document.querySelectorAll(".item"));
+
+async function write(text) {
+  let index = 0;
+  while (index < text.length) {
+    await sleep(100);
+    index++;
+    dataText.innerText = text.substring(0, index);
+  }
+}
+
+async function erase() {
+  let index = dataText.innerText.length;
+  while (index >= dataText.innerText.length) {
+    await sleep(100);
+    dataText.innerHTML = dataText.innerText.substring(
+      0,
+      dataText.innerHTML.length - 2
+    );
+    index--;
+  }
+}
+
+(async () => {
+  for (let i = 0; i < stringsContainer.length; i++) {
+    await sleep(2000);
+    write(stringsContainer[i].textContent);
+    await sleep(2000);
+    erase();
+  }
+})();
+// (async () => {
+//   await sleep(4000);
+//   await erase();
+// })();
