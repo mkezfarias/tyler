e1c2a80afcf70ed0e269d5e9bb6ec3d56005b0b6
Luis Puente <puente.luism@gmail.com>
2020-08-04 23:51:00 -0500

    Add index elements

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
