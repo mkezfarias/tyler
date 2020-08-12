95c88711f5b6f7c31e8f43b7cbe009cf6ae8b25a
Luis Puente <puente.luism@gmail.com>
2020-08-12 00:56:39 -0500

    Page layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
+    title: `Tyler’s latest posts:`,
     author: `Gatsby`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
