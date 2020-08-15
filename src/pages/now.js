0333c77e39bf26f957f90bf0e5ce224a858e365c
Tyler Vawser <TVawser@gmail.com>
2020-08-14 21:28:03 -0500

    Update now.js

--- src/pages/now.js
+++ src/pages/now.js
@@ -30,10 +30,10 @@ const nowContent = {
 
 const bigNow = [
   "1. Living in Little Rock, Arkansas",
-  "2. Building <a href=“https://www.apptegy.com/“>Apptegy</a>",
+  "2. Building <a href="https://www.apptegy.com/">Apptegy</a>",
   "3. Hiring thoughtful engineers in Little Rock & Monterrey",
   "4. Exploring digital minimalism & classical piano",
-  "5. Keeping up with 4 kids”,
+  "5. Keeping up with 4 kids",
 ]
 
 const onMyMind = [
