412825eac7dc9abf142082b12d4debc70f297fda
Tyler Vawser <TVawser@gmail.com>
2020-08-15 22:09:58 -0500

    reverting back to original calendly.js

--- src/pages/calendly.js
+++ src/pages/calendly.js
@@ -67,31 +67,6 @@ class Calendly extends React.Component {
           <Row>
       </Row>
         </div>
-      class Calendly extends React.Component {
-  componentDidMount() {
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.setAttribute('src',  'https://assets.calendly.com/assets/external/widget.js');
-    head.appendChild(script);
-  }
-
-  componentWillUnmount() {
-    // whatever you need to cleanup the widgets code
-  }
-
-  render(){
-    return (
-      <div>
-        <div id="schedule_form">
-          <div 
-            className="calendly-inline-widget"
-            data-url="https://calendly.com/tyler-apptegy/call-with-tyler"
-            style={{ minWidth: '320px', height: '580px' }} />
-        </div>
-      </div>
-    );
-  }
-}
         <Container fluid>
           <Col
             style={{
