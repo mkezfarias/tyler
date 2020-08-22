427423223b525816a809b032516b82dd5767e412
Luis Puente <puente.luism@gmail.com>
2020-08-22 14:28:43 -0500

    Fix modal menu on mobile

--- src/pages/calendly.js
+++ src/pages/calendly.js
@@ -37,9 +37,6 @@ class Calendly extends React.Component {
     this.state = {
       show: false,
     }
-
-    this.handleClose = () => this.setState({ show: false })
-    this.handleShow = () => this.setState({ show: true })
   }
 
   render() {
@@ -50,108 +47,45 @@ class Calendly extends React.Component {
           keywords={[`blog`, `Tyler Vawser`, `Tvawser`, `react`]}
         />
         <LogoFixedMobile />
-
-        <div className="my-0 py-0">
-          <Row
-            style={{}}
-            className=" calendly-widget d-flex justify-content-start align-items-end"
-          >
-            <Col
-              xl={7}
-              sm={10}
-              className="wider wsans w-medium pb-0 mb-0 line-height-1 mt-0 w-semibold align-center"
-            >
-              <CalendlyEmbed
-                className="mb-0 mx-auto"
-                account="tyler-apptegy"
-                eventName="call-with-tyler"
-              />
-            </Col>
-          </Row>
-          <Row
-            style={{
-              color: "var(--textNormal)",
-              background: `var(--gradient-background)`,
-              textAlign: `center`,
-              fontSize: `calc(20px + 2vw)`,
-              position: `fixed`,
-              top: `0`,
-              right: `0`,
-              bottom: `0`,
-              zIndex: `0`,
-            }}
-          >
-            <Col
-              xl={5}
-              sm={12}
-              className="wider wsans w-medium line-height-1  w-semibold align-center"
-            >
-              <Row
-                className="d-flex justify-content-end align-items-end"
-                style={{
-                  padding: `0 6vw`,
-                  textAlign: `end`,
-                  position: `fixed`,
-                  bottom: `5vh`,
-                  marginRight: `3vw`,
-                }}
-              >
-                <Col xl={12}>You can also check out my social media</Col>
-                <Col
-                  xl={{ span: 8, offset: 3 }}
-                  className="d-flex  justify-content-between pt-3 "
-                >
-                  <a
-                    href="https://twitter.com/Tvaw"
-                    className="d-flex align-items-end justify-content-between"
-                  >
-                    <img
-                      src={twitterIMG}
-                      alt="Link to Tyler's twitter"
-                      className="d-flex align-self-end"
-                    />
-                  </a>
-
-                  <a href="https://www.linkedin.com/in/tylervawser/">
-                    <img
-                      className="d-flex align-self-end"
-                      src={linkedinIMG}
-                      alt="Link to Tyler's Linkedin profile"
-                    />
-                  </a>
-
-                  <a href="https://twitter.com/Tvaw">
-                    <img
-                      src={mailIMG}
-                      alt="Link to Tyler's twitter"
-                      className="d-flex align-self-end"
-                    />
-                  </a>
-                </Col>
-              </Row>
-              <Row
-                style={{
-                  padding: `6vw`,
-                  marginBottom: `20vh`,
-                  textAlign: `end`,
-                  position: `fixed`,
-                  bottom: `20px`,
-                }}
-              ></Row>
-            </Col>
-          </Row>
-        </div>
-        <Container fluid>
+        <Col xl={6} sm={11} className="">
+          <CalendlyEmbed account="tyler-apptegy" eventName="call-with-tyler" />
+        </Col>
+        <div className="sidebar-fixed">
+          <Col xl={5} className="">
+            You can also check out my social media
+          </Col>
           <Col
-            style={{
-              background: "var(--gradient-background)",
-              width: `40vw`,
-              margin: `6vh -10vw 0 -10vw`,
-            }}
-            className=""
-          ></Col>
-          <Col xl={{ span: 6, offset: 1 }} className="mb-5 pb-5"></Col>
-        </Container>
+            xl={{ span: 8, offset: 3 }}
+            className="d-flex  justify-content-between pt-3 "
+          >
+            <a
+              href="https://twitter.com/Tvaw"
+              className="d-flex align-items-end justify-content-between"
+            >
+              <img
+                src={twitterIMG}
+                alt="Link to Tyler's twitter"
+                className="d-flex align-self-end"
+              />
+            </a>
+
+            <a href="https://www.linkedin.com/in/tylervawser/">
+              <img
+                className="d-flex align-self-end"
+                src={linkedinIMG}
+                alt="Link to Tyler's Linkedin profile"
+              />
+            </a>
+
+            <a href="https://twitter.com/Tvaw">
+              <img
+                src={mailIMG}
+                alt="Link to Tyler's twitter"
+                className="d-flex align-self-end"
+              />
+            </a>
+          </Col>
+        </div>
       </Layout>
     )
   }
