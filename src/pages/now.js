ed3b422bafb5e9a4d67c13de93ba55af52482b27
Luis Puente <puente.luism@gmail.com>
2020-10-01 13:39:36 -0500

    remove css import form now and removed pretty url from netlify

--- src/pages/now.js
+++ src/pages/now.js
@@ -1,10 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-
 import { Container, Row, Col } from "react-bootstrap"
-import "../styles/global.scss"
 import arrowIMG from "../images/arrow.svg"
 import LogoFixedMobile from "../components/LogoFixedMobile"
 
